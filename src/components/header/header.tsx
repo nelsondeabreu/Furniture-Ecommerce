5854e4cb398d1092a9b618e063a3bca19e004e27
Nelson de Abreu <nelsonabreu118@gmail.com>
2024-07-15 20:51:39 +0100

    created project

---
+++ src/components/header/header.tsx
@@ -0,0 +1,24 @@
+
+export function Header() {
+    return(
+        <div className="flex flex-col w-[100%]">
+            <div>
+                <ul className="flex gap-4">
+                    <li>
+                        +xx xxx xxx x
+                    </li>
+                    <li>
+                        xxxxxxxx@xxxx.com
+                    </li>
+                </ul>
+
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
