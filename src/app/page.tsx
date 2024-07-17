9074303cdf04f2c3f05d97deaf5ae667b0969421
Nelson de Abreu <nelsonabreu118@gmail.com>
2024-07-17 19:25:49 +0100

    Header created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,9 @@ import { Header } from "@/components/header/header";
 
 export default function Home() {
   return (
-    <main className="flex flex-col w-screen h-screen items-center">
-      <div>
-        <Header/>
-      </div>
+    <main className="flex flex-col w-screen items-center px-16 py-4">
+      <Header/>
+
     </main>
   );
 }
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,22 +1,40 @@
+import { CircleChevronDown, GitCommitIcon, Mail, Phone, SquareCode, SquareTerminal, Star } from "lucide-react";
 
 export function Header() {
     return(
         <div className="flex flex-col w-[100%]">
-            <div>
+            <div className="flex justify-between border-b-2 border-gray-200 pb-4 mb-4">
                 <ul className="flex gap-4">
-                    <li>
-                        +xx xxx xxx x
+                    <li className="flex gap-1">
+                        <Phone color="#F5993C"/> <p>+xx xxx xxx x</p> 
                     </li>
-                    <li>
-                        xxxxxxxx@xxxx.com
+                    <li className="flex gap-1">
+                        <Mail color="#F5993C"/> <p>xxxxxxxx@xxxx.com</p> 
                     </li>
                 </ul>
 
-                <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
+                <ul className="flex gap-6">
+                    <li><Star/></li>
+                    <li><SquareTerminal/></li>
+                    <li><SquareCode/></li>
+                    <li><GitCommitIcon/></li>
+                </ul>
+            </div>
+
+            <div className="flex justify-between items-center text-sm">
+                <h1 className="text-3xl font-extrabold">Furniture</h1>
+                <ul className="flex gap-8 text-gray-300 justify-center items-center">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li className="flex justify-center items-center gap-1">Furniture <CircleChevronDown size="15"/></li>
+                    <li>Car Catalogue</li>
+                    <li>FAQ</li>
+                </ul>
+                <ul className="flex gap-8 text-[#F5993C] justify-center items-center">
+                    <li>Sign In</li>
+                    <li>
+                        <button type="button" className="bg-[#F5993C]  text-white py-2 px-4 rounded-full">Sign Up</button>
+                    </li>
                 </ul>
             </div>
         </div>
