00f92e64726c038c3d56d5583f8716689ac69119
Nelson de Abreu <nelsonabreu118@gmail.com>
2024-07-22 22:58:40 +0100

    Hero section created & category section update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import { Header } from "@/components/header/header";
-import { Hero_Section } from "@/components/cards/cards";
+import { Category_Section, Hero_Section } from "@/components/cards/cards";
 
 export default function Home() {
   return (
-    <main className="flex flex-col w-screen h-screen items-center px-16 py-4 gap-8 bg-slate-50 overflow-x-hidden">
+    <main className="flex flex-col w-screen h-screen items-center px-16 py-4 gap-8 overflow-x-hidden">
       <Header/>
       <Hero_Section/>
+      {<Category_Section/>}
     </main>
   );
 }
--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -1,6 +1,9 @@
 import { BadgeCheck, BaggageClaim, Handshake, Locate, PlayCircle } from "lucide-react";
 import Image from "next/image";
 import image1 from "../../../public/image1.jpeg"
+import image_table from "../../../public/image_table.png"
+import image_table_pc from "../../../public/image_table_pc.png"
+import image_sofa from "../../../public/image_sofa.png"
 
 
 export function Hero_Section() {
@@ -26,10 +29,10 @@ export function Hero_Section() {
                     </ul>
                 </div>
 
-                <div className="w-[40rem] h-[25rem] rounded-3xl overflow-hidden">
+                <div className="w-[40rem] h-[30rem] rounded-3xl overflow-hidden">
                     <Image src={image1} alt="image" className="w-[100%] h-[100%] "/>
                 </div>
-                <div className="w-[70%] absolute top-[23rem] rounded-full py-2 px-6 bg-white">
+                <div className="w-[70%] absolute top-[28em] rounded-full py-2 px-6 bg-white shadow-xl">
                     <ul className="flex justify-between items-center gap-8 text-gray-400">
                         <li>
                             <h1 className="font-semibold">Confort</h1>
@@ -73,6 +76,42 @@ export function Hero_Section() {
                     <li className="text-3xl font-extrabold">Makita</li>
                     <li className="text-3xl font-extrabold">3M</li>
                 </ul>
+        </div>
+    )
+}
+
+export function Category_Section() {
+    return(
+        <div className="w-[100%]">
+            <ul className="flex justify-between items-center">
+                <li className="flex bg-[#EDEDED7D] px-4 rounded-2xl  justify-center items-center">
+                    <div className="flex flex-col gap-4">
+                        <h1 className="font-semibold text-xl">Dining Tables</h1>
+                        <button className="bg-white font-bold text-sm p-2 rounded-full">See More</button>
+                    </div>
+                    <div className="w-52 h-40 px-8">
+                        <Image className="w-[100%] h-[100%]" src={image_table} alt="image"/>
+                    </div>
+                </li>
+                <li className="flex bg-[#EDEDED7D] px-4 rounded-2xl  justify-center items-center">
+                    <div className="flex flex-col gap-4">
+                        <h1 className="font-semibold text-xl">Chairs</h1>
+                        <button className="bg-white font-bold text-sm p-2 rounded-full">See More</button>
+                    </div>
+                    <div className="w-52 h-40 px-8">
+                        <Image className="w-[100%] h-[100%]" src={image_sofa} alt="image"/>
+                    </div>
+                </li>
+                <li className="flex bg-[#EDEDED7D] px-4 rounded-2xl  justify-center items-center">
+                    <div className="flex flex-col gap-4">
+                        <h1 className="font-semibold text-xl">Office Desks</h1>
+                        <button className="bg-white font-bold text-sm p-2 rounded-full">See More</button>
+                    </div>
+                    <div className="w-52 h-40 px-8">
+                        <Image className="w-[100%] h-[100%]" src={image_table_pc} alt="image"/>
+                    </div>
+                </li>
+            </ul>
         </div>
     )
 }
